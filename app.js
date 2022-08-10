1504947cb7d63a3d699c9d50a9312de8c54d4af0
NA-Siddiky <siddiky.academic@gmail.com>
2022-08-11 01:56:52 +0600

    code refectoring

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const { addUser, crudOperation, queryData, findData, setterGetter } = require('./controllers/userController');
+const {
+    addUser,
+    crudOperation,
+    queryData,
+    findData,
+    setterGetter,
+} = require('./controllers/userController');
+
 const app = express();
 
 const PORT = 6060;
-require('./models')
+require('./models');
 
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    res.send('Home Page');
 });
 
 app.get('/add', addUser);
 app.get('/crud', crudOperation);
 app.get('/query', queryData);
 app.get('/finder', findData);
-app.get("/setter-getter", setterGetter)
+app.get('/setter-getter', setterGetter);
 
 app.listen(PORT, () => {
     console.log(`app is running at http://localhost:${PORT}`);
-})
+});
